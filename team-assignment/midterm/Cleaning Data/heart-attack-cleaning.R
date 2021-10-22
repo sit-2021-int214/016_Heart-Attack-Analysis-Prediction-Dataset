b3ea7affad8a19d442d40c9f8fa5ce8e270d8c09
neko7687 <50837502+neko7687@users.noreply.github.com>
2021-10-22 17:24:33 +0700

    readme.md

---
+++ team-assignment/midterm/Cleaning Data/heart-attack-cleaning.R
@@ -0,0 +1,10 @@
+## Library
+library(stringr)
+library(readr)
+library(assertive)
+library(dplyr)
+
+##Dataset
+heart <- read.csv("https://raw.githubusercontent.com/sit-2021-int214/016_Heart-Attack-Analysis-Prediction-Dataset/b432f56eabee0a2092e7f08f5846b774e0290e21/heart-attack-analysis-prediction-dataset.csv")
+
+heart
