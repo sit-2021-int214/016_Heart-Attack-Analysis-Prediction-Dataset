875240120f3ef473260b1d9afb14567e1408ed47
neko7687 <50837502+neko7687@users.noreply.github.com>
2021-10-22 19:45:53 +0700

    Explore

--- team-assignment/midterm/Data Exploration/data exploration.R
+++ team-assignment/midterm/Data Exploration/data exploration.R
@@ -4,6 +4,7 @@ library(readr)
 library(assertive)
 library(dplyr)
 
+heart <- read.csv("https://raw.githubusercontent.com/sit-2021-int214/016_Heart-Attack-Analysis-Prediction-Dataset/b432f56eabee0a2092e7f08f5846b774e0290e21/heart-attack-analysis-prediction-dataset.csv")
 
 glimpse(heart)
 
