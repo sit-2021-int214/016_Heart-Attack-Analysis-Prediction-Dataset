38be27e5d97444922d31776bb4ecaaa5080f85b3
neko7687 <50837502+neko7687@users.noreply.github.com>
2021-10-22 19:43:00 +0700

    Explore

--- team-assignment/midterm/Cleaning Data/heart-attack-cleaning.R
+++ team-assignment/midterm/Cleaning Data/heart-attack-cleaning.R
@@ -4,7 +4,16 @@ library(readr)
 library(assertive)
 library(dplyr)
 
-##Dataset
+#Data set
 heart <- read.csv("https://raw.githubusercontent.com/sit-2021-int214/016_Heart-Attack-Analysis-Prediction-Dataset/b432f56eabee0a2092e7f08f5846b774e0290e21/heart-attack-analysis-prediction-dataset.csv")
 
-heart
+#Remove Duplicate
+heart <- heart %>% distinct()
+
+#Check NA
+length(which(is.na(heart)))
+
+#
+heart$sex[heart$sex == 0] <- "female"
+heart$sex[heart$sex == 1] <- "male"
+heart$sex
---
+++ team-assignment/midterm/Data Exploration/data exploration.R
@@ -0,0 +1,12 @@
+#Library
+library(stringr)
+library(readr)
+library(assertive)
+library(dplyr)
+
+
+glimpse(heart)
+
+summary(heart)
+
+
