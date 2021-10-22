3efb96f21375b7b9b96128949e2e83c592da5bcb
thanakritcankha <68786794+thanakritcankha@users.noreply.github.com>
2021-10-23 01:01:36 +0700

    s

--- team-assignment/midterm/Cleaning Data/heart-attack-cleaning.R
+++ team-assignment/midterm/Cleaning Data/heart-attack-cleaning.R
@@ -1,32 +1,5 @@
-## Library
-library(stringr)
-library(readr)
-library(assertive)
-library(dplyr)
-
-#Data set
-heart <- read.csv("https://raw.githubusercontent.com/sit-2021-int214/016_Heart-Attack-Analysis-Prediction-Dataset/b432f56eabee0a2092e7f08f5846b774e0290e21/heart-attack-analysis-prediction-dataset.csv")
-
-#Remove Duplicate
-heart <- heart %>% distinct()
-
-#Check NA
-length(which(is.na(heart)))
-
-#Change datatype Numberic to Charactors 
-#Chagne 0 to feamale and 1 to male
-heart$sex[heart$sex == 0] <- "female"
-heart$sex[heart$sex == 1] <- "male"
-
-#Remove unused columns
-##Remove oldpeak columns
-heart$oldpeak <- NULL
-##Remove slp columns
-heart$slp <- NULL
-##Remove thall columns
-heart$thall <- NULL
-
-#Export to CSV
-write.csv(heart,".\heart-attack-cleaning.csv")
-
+exAngina <- count(heart %>% filter(exng == 1))
+print(exAngina)
+avg_exAngina <- exAngina / count(heart)
+paste(round(100*avg_exAngina , 2) , "%")
 
