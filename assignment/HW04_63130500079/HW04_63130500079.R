f9641bc57b831ced408b4e51aec54ed8817935e7
neko7687 <50837502+neko7687@users.noreply.github.com>
2021-10-25 15:43:02 +0700

    Almost last

--- assignment/HW04_63130500079/HW04_63130500079.R
+++ assignment/HW04_63130500079/HW04_63130500079.R
@@ -15,11 +15,14 @@ topbook <- read.csv("https://raw.githubusercontent.com/safesit23/INT214-Statisti
 #View dataset
 glimpse(topbook)
 View(topbook)
-
+#?????? na
 is.na(topbook) %>% table()
-
+#summary
 topbook %>% select(-Book_title, -Description, -Type,) %>% summary()
-
+#????????????????????? reviews ????????? char ???????????? number
+topbook$Reviews <- gsub(",","",topbook$Reviews)
+topbook$Reviews <- as.numeric(topbook$Reviews)
+typeof(topbook$Reviews)
 #Part2
 ## ??????????????????????????? output ??????????????????????????????????????????????????????????????? ?????????????????????????????? head ??????????????????????????????
 ##select
@@ -27,27 +30,38 @@ head(topbook %>% select(Book_title,Price,Rating))
 ##filter
 head(topbook %>% select(Book_title,Price) %>% filter(Price < 30))
 ##arrange
-topbook %>% select(Book_title,Rating) %>% arrange(Rating)
+head(topbook %>% select(Book_title,Rating) %>% arrange(desc(Rating), .by_group = TRUE)) 
 
-#1
-topbook %>% count(Type) %>% arrange(n)
+#Part3
 
-#2
-topbook %>% select(-Description) %>% filter(Price == min(Price)) 
+#3.1
+topbook %>% count(Type) %>% arrange(desc(n))
 
-#3
-topbook %>% group_by(Type) %>% summarise(mean_price = mean(Price))
+#3.2
+topbook %>% select(Book_title , Rating , Price) %>% filter(Price == min(Price)) 
 
-#4
-topbook %>% select(Book_title,Rating,Price) %>% arrange(desc(Rating)) %>% head(n=10)
+#3.3
+topbook %>% select(Book_title,Rating) %>% arrange(desc(Rating)) %>% head(n=10)
 
-#5
-topbook %>% filter(Price < mean(Price)) %>% select(Book_title, Price) %>% arrange(desc(Price)) %>% head(n=5) 
+#3.4
+topbook %>% select(Book_title,Rating,Price) %>% arrange(desc(Price)) %>% head(n=10) 
 
-#6
-topbook %>% filter(Reviews > 1000) %>% select(Reviews,Book_title,Price) %>% arrange(desc(Reviews))
+#3.5
+topbook %>% group_by(Type) %>% summarise(avr_price = mean(Price))
 
-topbook %>% ggplot(aes(x=Type)) + geom_bar() + ggtitle("Number of type book")
+#3.6
+topbook %>% select(Reviews,Book_title,Price) %>% arrange(desc(Reviews)) %>% head(n=10)
 
-topbook %>% filter(Price < mean(Price)) %>% ggplot(aes(x=Price,y=Rating))+ geom_point() +
-  geom_point(aes(color=Type))+ ggtitle("Relation between Rating and Price less than mean") + geom_smooth(method="lm")
+#3.7
+sum(topbook$Reviews)
+
+#3.8
+topbook %>% filter(Rating > "4.5") %>% select(Reviews,Book_title,Rating) %>% arrange(desc(Reviews)) %>% head(n=5) 
+
+
+#Part4
+#4.1
+topbook %>% filter(Rating > "4.5") %>% ggplot(aes(x=Rating)) + geom_bar() + ggtitle("Number of type book")
+
+topbook %>% arrange(desc(Reviews)) %>% head(n=5) %>% ggplot(aes(x=Price,y=Rating))+ geom_point() +
+  geom_point(aes(color=Type))+ ggtitle("Relation between Rating and Price of book 4.5 Star+") + geom_smooth(method="lm")
